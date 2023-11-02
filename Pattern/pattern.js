53aaef14341dff206abf434936f5be224d4a4366
Krishna <1904krishna@gmail.com>
2023-11-02 18:23:38 +0530

    added more

--- Pattern/pattern.js
+++ Pattern/pattern.js
@@ -1,3 +1,4 @@
+const prompt = require("prompt-sync")({ sigint: true });
 /*Patterns in JavaScript
 ********************************************
 Example 1: Triangle Pattern - I
@@ -19,49 +20,91 @@ In the example above, we have 5 rows. For 5 rows, we need an external loop that 
 For each row, we need to print numbers from num = 1 to num = n.
 Example : For the 5th row, n = 5. The internal loop goes from num = 1 to num = 5. The 5th row becomes: 1 2 3 4 5
 Code:
+
+var rows_num = prompt("Enter num of rows: ");
+var num = ""
+
+for (var i = 1; i <= rows_num; i++) {
+  for (var j = 1; j <= i; j++) {
+    num += j;
+  }
+  num += "\n";
+}
+
+console.log(num);
+
+********************************************
+
+Example 2: Triangle Pattern - II
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+
+var input = prompt("enter the value: ");
+var num = "";
+
+for (let i = 1; i <= input; i++) {
+    for (let j = 1; j <= i; j++){
+        num += i;
+    }
+    num += "\n";
+}
+console.log(num);
+
+********************************************
+
+Example 3 : Triangle Pattern - III
+
+1
+2 3
+4 5 6
+7 8 9 10
+
+
+var input = prompt("Enter the value: ");
+var variable = 1
+var num = "";
+for (let i = 1; i <= input; i++) {
+  for (let j = 1; j <= i; j++) {
+    num += variable+" ";
+    variable++;
+  }
+  num += "\n";
+}
+console.log(num);
+
+//********************************************
+Example 4: Reverse Triangle - I
+
+12345
+1234
+123
+12
+1
+*/ /*
+let rows = 90;
+// pattern variable carries the final pattern in string format
+let pattern = "";
+// outer loop runs for `rows` no. of times
+for (let n = 1; n <= rows; n++) {
+   for (let num = 1; num <= 6 - n; num++) {
+      pattern += num;
+   }
+   pattern += "\n";
+}
+console.log(pattern);
 */
+var input = prompt("Enter the value: ");
+var row = parseInt(input)+ 1;
+var num = "";
+for (let i = 1; i <= input; i++) {
+  for (let j = 1; j <= row - i; j++) {
+    num += j;
+  }
+  num += "\n";
+}
+console.log(num);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// END OF THE EXERCISE
